571da303837bdfa752a6f78fd308ce789126b843
Ajeet-Singh-01 <freeaj09@gmail.com>
2024-08-27 15:36:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-
+        const apiKey;
+        const apiurl;
         const searchBox=document.querySelector(".search input");
         const searchBtn=document.querySelector(".search button");
-        const weatherIcon=document.querySelector(".weather-icon");
+        const weatherIcon=document.querySelector(".Weather-icon");
 
         async function checkweather(city){
             const response=await fetch(apiUrl + city + `&appid=${apikey}`);
             if(response.status==404){
                 document.querySelector(".error").style.display="block";
-                document.querySelector(".error").style.display="none";
+                document.querySelector(".Weathor").style.display="none";
             }
             else{
 
@@ -19,7 +20,7 @@
             document.querySelector(".city").innerHTML=data.name;
             document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°c";
             document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
-            document.querySelector(".Wind").innerHTML=data.Wind.Wind_speed + "km/h";
+            document.querySelector(".Wind").innerHTML=data.wind.speed + "km/h";
             
             if(data.weather[0].main== "Clouds"){
                 weatherIcon.src="images/th (1).jpeg";
